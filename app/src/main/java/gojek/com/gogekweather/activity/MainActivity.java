e1bc1e6a976aaa53092ad7ba9713caa21b1ff9ed
Ravichandra S V <ravichandra.sv@accionlabs.com>
2018-05-12 10:02:28 +0530

    Moved

--- app/src/main/java/gojek/com/gogekweather/activity/MainActivity.java
+++ app/src/main/java/gojek/com/gogekweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package gojek.com.gogekweather;
+package gojek.com.gogekweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import gojek.com.gogekweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
